270845b81d77841573e4762a36fb26f37cd056b8
Tony Mamo <tmamo13991@gmail.com>
2023-06-23 09:24:43 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import image from "@astrojs/image"
+import prefetch from "@astrojs/prefetch"
 
-import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), prefetch()]
-});
+  site: "https://robb4livonia.netlify.app",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    prefetch(),
+    sitemap(),
+  ],
+})
