cac69df6560efd96aa481eff4caa4af198abc8f3
Tony Mamo <tmamo13991@gmail.com>
2023-06-23 09:21:31 -0400

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
-import image from "@astrojs/image"
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
-})
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), prefetch()]
+});
